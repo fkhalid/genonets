18d738e8256f48e359268f4ec1c9e736956e698d
Fahad Khalid <fahad.khalid@gmail.com>
2016-02-23 21:42:38 +0100

    Updated version for pypi upload.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(	name='genonets',
-		version='1.0.0',
+		version='1.0.2',
 		description='Framework for creating and analyzing genotype networks from data.',
 		author='Fahad Khalid',
 		author_email='fahad.khalid@ieu.uzh.ch',
